aea2aeb935c5f709a799ff079a780fba2c8cca8a
Benjamin <benjamin.guy.thomas@gmail.com>
2021-04-03 08:22:12 +0200

    Render failure, see "this does not execute"

--- src/main/java/app/controllers/HelloController.java
+++ src/main/java/app/controllers/HelloController.java
@@ -1,6 +1,12 @@
 package app.controllers;
 
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.output.StringOutput;
+import gg.jte.resolve.DirectoryCodeResolver;
 import org.javalite.activeweb.AppController;
+
+import java.nio.file.Path;
 
 public class HelloController extends AppController {
     public HttpBuilder index() {
@@ -8,6 +14,21 @@ public class HelloController extends AppController {
     }
 
     public HttpBuilder world() {
-        return respond("Hello world from HelloController#world!");
+        return respond("Hello2 world from HelloController#world!");
+    }
+
+    public HttpBuilder world2() {
+        // Tell jte where your template files are located
+        var codeResolver = new DirectoryCodeResolver(Path.of("src", "main", "jte"));
+
+        // Create the template engine (usually once per application)
+        var templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+
+        // Render
+        var output = new StringOutput();
+        System.out.println("---> before render");
+        templateEngine.render("hello.jte", "World", output);
+        System.out.println("---> after render (this does not execute)");
+        return respond(output.toString()); // not sure if that'll work
     }
 }
